a78fb47bc48008dbd20d5173044e8140f167b1e1
SoliLab <31732875+SoliLab@users.noreply.github.com>
2021-09-14 22:09:25 +0800

    Add files via upload

---
+++ stock/stock_fund_hold.py
@@ -0,0 +1,168 @@
+# -*- coding:utf-8 -*-
+# /usr/bin/env python
+"""
+Date: 2021/5/15 20:09
+Desc: 东方财富网-数据中心-主力数据-基金持仓
+http://data.eastmoney.com/zlsj/2020-06-30-1-2.html
+"""
+import demjson
+import pandas as pd
+import requests
+
+
+def stock_report_fund_hold(symbol: str = "基金持仓", date: str = "20201231") -> pd.DataFrame:
+    """
+    东方财富网-数据中心-主力数据-基金持仓
+    http://data.eastmoney.com/zlsj/2020-12-31-1-2.html
+    :param symbol: choice of {"基金持仓", "QFII持仓", "社保持仓", "券商持仓", "保险持仓", "信托持仓"}
+    :type symbol: str
+    :param date: 财报发布日期, xxxx-03-31, xxxx-06-30, xxxx-09-30, xxxx-12-31
+    :type date: str
+    :return: 基金持仓数据
+    :rtype: pandas.DataFrame
+    """
+    symbol_map = {
+        "基金持仓": "1",
+        "QFII持仓": "2",
+        "社保持仓": "3",
+        "券商持仓": "4",
+        "保险持仓": "5",
+        "信托持仓": "6",
+    }
+    date = "-".join([date[:4], date[4:6], date[6:]])
+    url = "http://data.eastmoney.com/dataapi/zlsj/list"
+    params = {
+        "tkn": "eastmoney",
+        "ReportDate": date,
+        "code": "",
+        "type": symbol_map[symbol],
+        "zjc": "0",
+        "sortField": "Count",
+        "sortDirec": "1",
+        "pageNum": "1",
+        "pageSize": "50000",
+        "cfg": "jjsjtj",
+        "p": "1",
+        "pageNo": "1",
+    }
+    r = requests.get(url, params=params)
+    data_text = r.text
+    data_json = demjson.decode(data_text[data_text.find("{"):])
+    temp_df = pd.DataFrame(data_json["data"])
+    temp_df.reset_index(inplace=True)
+    temp_df["index"] = list(range(1, len(temp_df)+1))
+    if temp_df.empty:
+        return None
+    temp_df.columns = [
+        "序号",
+        "股票代码",
+        "股票简称",
+        "_",
+        "_",
+        "_",
+        "持有基金家数",
+        "持股变化",
+        "持股总数",
+        "持股市值",
+        "_",
+        "_",
+        "持股变动数值",
+        "持股变动比例",
+    ]
+    temp_df = temp_df[[
+        "序号",
+        "股票代码",
+        "股票简称",
+        "持有基金家数",
+        "持股总数",
+        "持股市值",
+        "持股变化",
+        "持股变动数值",
+        "持股变动比例",
+    ]]
+    return temp_df
+
+
+def stock_report_fund_hold_detail(symbol: str = "005827", date: str = "20201231") -> pd.DataFrame:
+    """
+    东方财富网-数据中心-主力数据-基金持仓-明细
+    http://data.eastmoney.com/zlsj/ccjj/2020-12-31-008286.html
+    :param symbol: 基金代码
+    :type symbol: str
+    :param date: 财报发布日期, xxxx-03-31, xxxx-06-30, xxxx-09-30, xxxx-12-31
+    :type date: str
+    :return: 基金持仓-明细数据
+    :rtype: pandas.DataFrame
+    """
+    date = "-".join([date[:4], date[4:6], date[6:]])
+    url = "http://datainterface3.eastmoney.com/EM_DataCenter_V3/api/ZLCCMX/GetZLCCMX"
+    params = {
+        "js": "datatable8848106",
+        "tkn": "eastmoney",
+        "SHType": "1",
+        "SHCode": symbol,
+        "SCode": "",
+        "ReportDate": date,
+        "sortField": "SCode",
+        "sortDirec": "1",
+        "pageNum": "1",
+        "pageSize": "500",
+        "cfg": "ZLCCMX",
+        "_": "1611579153269",
+    }
+    r = requests.get(url, params=params)
+    data_text = r.text
+    data_json = demjson.decode(data_text[data_text.find("{"):-1])
+    temp_df = pd.DataFrame(data_json["Data"][0])
+    temp_df = temp_df["Data"].str.split("|", expand=True)
+    temp_df.reset_index(inplace=True)
+    temp_df["index"] = range(1, len(temp_df)+1)
+    temp_df.columns = [
+        "序号",
+        "股票代码",
+        "股票简称",
+        "_",
+        "_",
+        "_",
+        "_",
+        "_",
+        "_",
+        "_",
+        "持股数",
+        "持股市值",
+        "占总股本比例",
+        "占流通股本比例",
+    ]
+    temp_df = temp_df[[
+        "序号",
+        "股票代码",
+        "股票简称",
+        "持股数",
+        "持股市值",
+        "占总股本比例",
+        "占流通股本比例",
+    ]]
+    return temp_df
+
+
+if __name__ == "__main__":
+    stock_report_fund_hold_df = stock_report_fund_hold(symbol="基金持仓", date="20201231")
+    print(stock_report_fund_hold_df)
+
+    stock_report_fund_hold_df = stock_report_fund_hold(symbol="QFII持仓", date="20201231")
+    print(stock_report_fund_hold_df)
+
+    stock_report_fund_hold_df = stock_report_fund_hold(symbol="社保持仓", date="20201231")
+    print(stock_report_fund_hold_df)
+
+    stock_report_fund_hold_df = stock_report_fund_hold(symbol="券商持仓", date="20201231")
+    print(stock_report_fund_hold_df)
+
+    stock_report_fund_hold_df = stock_report_fund_hold(symbol="保险持仓", date="20201231")
+    print(stock_report_fund_hold_df)
+
+    stock_report_fund_hold_df = stock_report_fund_hold(symbol="信托持仓", date="20201231")
+    print(stock_report_fund_hold_df)
+
+    stock_report_fund_hold_detail_df = stock_report_fund_hold_detail(symbol="005827", date="20201231")
+    print(stock_report_fund_hold_detail_df)
---
+++ stock/stock_summary.py
@@ -0,0 +1,158 @@
+# -*- coding:utf-8 -*-
+# /usr/bin/env python
+"""
+Date: 2021/7/6 14:31
+Desc: 股票数据-总貌-市场总貌
+股票数据-总貌-成交概括
+http://www.szse.cn/market/overview/index.html
+http://www.sse.com.cn/market/stockdata/statistic/
+"""
+from io import BytesIO
+
+import demjson
+import pandas as pd
+import requests
+
+
+def stock_szse_summary(date: str = "20200619") -> pd.DataFrame:
+    """
+    深证证券交易所-总貌
+    http://www.szse.cn/market/overview/index.html
+    :param date: 最近结束交易日
+    :type date: str
+    :return: 深证证券交易所-总貌
+    :rtype: pandas.DataFrame
+    """
+    url = "http://www.szse.cn/api/report/ShowReport"
+    params = {
+        "SHOWTYPE": "xlsx",
+        "CATALOGID": "1803_sczm",
+        "TABKEY": "tab1",
+        "txtQueryDate": "-".join([date[:4], date[4:6], date[6:]]),
+        "random": "0.39339437497296137",
+    }
+    r = requests.get(url, params=params)
+    temp_df = pd.read_excel(BytesIO(r.content))
+    temp_df["证券类别"] = temp_df["证券类别"].str.strip()
+    temp_df.iloc[:, 2:] = temp_df.iloc[:, 2:].applymap(lambda x: x.replace(",", ""))
+    return temp_df
+
+
+def stock_sse_summary() -> pd.DataFrame:
+    """
+    上海证券交易所-总貌
+    http://www.sse.com.cn/market/stockdata/statistic/
+    :return: 上海证券交易所-总貌
+    :rtype: pandas.DataFrame
+    """
+    url = "http://www.sse.com.cn/market/stockdata/statistic/"
+    r = requests.get(url)
+    r.encoding = "utf-8"
+    big_df = pd.DataFrame()
+    temp_list = ["总貌", "主板", "科创板"]
+    for i in range(len(pd.read_html(r.text))):
+        for j in range(0, 2):
+            inner_df = pd.read_html(r.text)[i].iloc[:, j].str.split("  ", expand=True)
+            inner_df["item"] = temp_list[i]
+            big_df = big_df.append(inner_df)
+    big_df.dropna(how="any", inplace=True)
+    big_df.columns = ["item", "number", "type"]
+    big_df = big_df[["type", "item", "number"]]
+    return big_df
+
+
+def stock_sse_deal_daily(date: str = "20210325") -> pd.DataFrame:
+    """
+    上海证券交易所-数据-股票数据-成交概况-股票成交概况-每日股票情况
+    http://www.sse.com.cn/market/stockdata/overview/day/
+    :return: 每日股票情况
+    :rtype: pandas.DataFrame
+    """
+    url = "http://query.sse.com.cn/commonQuery.do"
+    params = {
+        "jsonCallBack": "jsonpCallback89906",
+        "searchDate": "-".join([date[:4], date[4:6], date[6:]]),
+        "sqlId": "COMMON_SSE_SJ_GPSJ_CJGK_DAYCJGK_C",
+        "stockType": "90",
+        "_": "1616744620492",
+    }
+    headers = {
+        "Referer": "http://www.sse.com.cn/",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36",
+    }
+    r = requests.get(url, params=params, headers=headers)
+    data_text = r.text
+    data_json = demjson.decode(data_text.strip("\r\njsonpCallback89906(")[:-1])
+    temp_df = pd.DataFrame(data_json["result"])
+    temp_df = temp_df.T
+    temp_df.reset_index(inplace=True)
+    temp_df.columns = [
+        "单日情况",
+        "主板A",
+        "股票",
+        "主板B",
+        "_",
+        "股票回购",
+        "科创板",
+    ]
+    temp_df = temp_df[
+        [
+            "单日情况",
+            "股票",
+            "主板A",
+            "主板B",
+            "科创板",
+            "股票回购",
+        ]
+    ]
+    temp_df["单日情况"] = [
+        "流通市值",
+        "流通换手率",
+        "平均市盈率",
+        "_",
+        "市价总值",
+        "_",
+        "换手率",
+        "_",
+        "挂牌数",
+        "_",
+        "_",
+        "_",
+        "_",
+        "_",
+        "成交笔数",
+        "成交金额",
+        "成交量",
+        "次新股换手率",
+        "_",
+        "_",
+    ]
+    temp_df = temp_df[temp_df["单日情况"] != "_"]
+    temp_df["单日情况"] = temp_df["单日情况"].astype("category")
+    list_custom_new = [
+        "挂牌数",
+        "市价总值",
+        "流通市值",
+        "成交金额",
+        "成交量",
+        "成交笔数",
+        "平均市盈率",
+        "换手率",
+        "次新股换手率",
+        "流通换手率",
+    ]
+    temp_df["单日情况"].cat.set_categories(list_custom_new)
+    temp_df.sort_values("单日情况", ascending=True, inplace=True)
+    temp_df.reset_index(drop=True, inplace=True)
+    return temp_df
+
+
+if __name__ == "__main__":
+    stock_szse_summary_df = stock_szse_summary(date="20200619")
+    print(stock_szse_summary_df)
+
+    stock_sse_summary_df = stock_sse_summary()
+    print(stock_sse_summary_df)
+
+    stock_sse_deal_daily_df = stock_sse_deal_daily(date="20201111")
+    print(stock_sse_deal_daily_df)
